072242c60c27ed67bb8c87d826dbe61ccc8a3d68
TechEntrepreneur-dev <tech.entreprenuer.hd@gmail.com>
2025-06-23 16:04:13 +0530

    update My Portfolio/Python Task 1.py

--- My Portfolio/Python Task 1.py
+++ My Portfolio/Python Task 1.py
@@ -1,8 +1,8 @@
-name = Tech Entrepreneur
+name = "Tech Entrepreneur"
 age = 18
 height = 5.4
 is_Genius = True
 
 print(f"Your name is {name}")
 print(f"You are {age}")
-print(f"You are {height}feet tall")
+print(f"You are {height} feet tall")
